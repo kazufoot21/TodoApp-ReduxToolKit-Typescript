a002a2c90a5880855adb2925b91c92364537bf11
kazufoot21 <kazufoot21@outlook.jp>
2021-12-25 19:44:56 +0900

    taskItem comp

---
+++ src/features/task/taskItem/taskItem.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import EventIcon from '@mui/icons-material/Event';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import styles from './taskItem.module.scss';
+
+interface PropTypes {
+  task: {
+    id: number;
+    title: string;
+    completed: boolean;
+  };
+}
+const taskItem: React.FC<PropTypes> = (props) => {
+  const { task } = props;
+  return (
+    <div className={styles.root}>
+      <div className={styles.title}>
+        <EventIcon />
+        <div className={styles.title_text}>{task.title}</div>
+      </div>
+      <div className={styles.right_item}>
+        <Checkbox
+          checked={task.completed}
+          onClick={() => {
+            console.log(`check${task.id}`);
+          }}
+          className={styles.checkbox}
+        />
+        <button
+          onClick={() => {
+            console.log(`edit ${task.id}`);
+          }}
+          className={styles.edit_button}
+        >
+          <EditIcon className={styles.icon} />
+        </button>
+        <button
+          onClick={() => {
+            console.log(`delete${task.id}`);
+          }}
+          className={styles.delete_button}
+        >
+          <DeleteIcon className={styles.icon} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default taskItem;
