5c04be103d8ba9c0441281ec1ab8e93218ed66a5
kazufoot21 <kazufoot21@outlook.jp>
2021-12-25 09:02:07 +0900

    ready for development

---
+++ src/components/Sample.tsx
@@ -0,0 +1,14 @@
+const Sample = () => {
+  return (
+    <div>
+      <button
+        className="button"
+        onClick={() => {
+          console.log('button');
+        }}
+      ></button>
+    </div>
+  );
+};
+
+export default Sample;
