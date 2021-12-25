20d4ffa27732af23310e19d08b3c0aa9b1006b1d
kazufoot21 <kazufoot21@outlook.jp>
2021-12-25 09:16:33 +0900

    create app.module.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './App.module.scss';
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return <div>App---OKOK</div>;
 };
 
 export default App;
