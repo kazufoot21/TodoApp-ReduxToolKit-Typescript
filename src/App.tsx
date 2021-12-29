2da5f9ce3c00868f85870302b133a8602fa80732
kazufoot21 <kazufoot21@outlook.jp>
2021-12-29 15:11:21 +0900

    setting initila firebase

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import Header from './components/header/Header';
 import TaskFrom from './features/task/taskForm/TaskFrom';
 import TaskList from './features/task/taskList/TaskList';
 
+import { auth } from './firebase';
+
 const App: React.FC = () => {
+  console.log(auth);
   return (
     <div className={styles.root}>
       <div className={styles.wrapper}>
